8a889e24dfd51d4f1aa1cb48768234ddc10a3dd1
Muhammad Shabri rabbani <shabri@mhs.unsyiah.ac.id>
2022-08-31 11:07:41 +0700

    Update listJournal.blade.php

--- resources/views/listJournal.blade.php
+++ resources/views/listJournal.blade.php
@@ -61,6 +61,7 @@
                                                             <td>
                                                                 <a class="btn-floating btn-small waves-effect waves-light"><i class="material-icons">remove_red_eye</i></a>
                                                                 <a class="btn-floating btn-small waves-effect waves-light green"><i class="material-icons">edit</i></a>
+                                                                <a class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">delete</i></a>
                                                             </td>
                                                         </tr>
                                                         <tr>
@@ -72,6 +73,7 @@
                                                             <td>
                                                                 <a class="btn-floating btn-small waves-effect waves-light"><i class="material-icons">remove_red_eye</i></a>
                                                                 <a class="btn-floating btn-small waves-effect waves-light green"><i class="material-icons">edit</i></a>
+                                                                <a class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">delete</i></a>
                                                             </td>
                                                         </tr>
                                                         <tr>
@@ -83,6 +85,7 @@
                                                             <td>
                                                                 <a class="btn-floating btn-small waves-effect waves-light"><i class="material-icons">remove_red_eye</i></a>
                                                                 <a class="btn-floating btn-small waves-effect waves-light green"><i class="material-icons">edit</i></a>
+                                                                <a class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">delete</i></a>
                                                             </td>
                                                         </tr>
                                                     </tbody>
